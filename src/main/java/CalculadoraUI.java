004aa3bbfe9720d34a964cb90f2c07766cab5d24
Francisco <fcocacerescolihuil@gmail.com>
2025-04-30 22:11:51 -0400

    show arithmetic operations options

--- src/main/java/CalculadoraUI.java
+++ src/main/java/CalculadoraUI.java
@@ -12,7 +12,7 @@ public class CalculadoraUI {
         int option;
         do {
             mostrarOpciones();
-            option = obtenerOpcion();
+            option = obtenerOpcion(1, 6);
             ejecutarOpcion(option);
         } while (option != 6);
     }
@@ -26,18 +26,18 @@ public class CalculadoraUI {
         System.out.println("[6] Salir");
     }
 
-    private static int obtenerOpcion() {
+    private static int obtenerOpcion(int min, int max) {
         System.out.print("Ingrese una opción: ");
+        int option;
         while (true) {
             String input = scanner.nextLine().trim();
-            int option;
             try {
                 option = Integer.parseInt(input);
-                if (option >= 1 && option <= 6) {
+                if (option >= min && option <= max) {
                     return option;
                 }
             } catch (Exception e) {}
-            System.out.print("Ingrese un número entero entre 1 y 6: ");
+            System.out.print("Ingrese un número entero entre " + min + " y " + max + ": ");
         }
     }
 
@@ -62,7 +62,53 @@ public class CalculadoraUI {
     }
 
     private static void operacionesAritmeticas() {
-        System.out.println("Operaciones aritméticas.");
+        mostrarOperacionesAritmeticas();
+        int option = obtenerOpcion(1, 9);
+        ejecutarOperacionAritmetica(option);
+    }
+
+    private static void mostrarOperacionesAritmeticas() {
+        System.out.println("[1] Sumar");
+        System.out.println("[2] Restar");
+        System.out.println("[3] Producto");
+        System.out.println("[4] Dividir");
+        System.out.println("[5] Mayor");
+        System.out.println("[6] Menor");
+        System.out.println("[7] Potencia");
+        System.out.println("[8] Porcentaje");
+        System.out.println("[9] Regresar al menú principal");
+    }
+
+    private static void ejecutarOperacionAritmetica(int option) {
+        switch (option) {
+            case 1:
+                System.out.println("Suma.");
+                break;
+            case 2:
+                System.out.println("Resta.");
+                break;
+            case 3:
+                System.out.println("Producto.");
+                break;
+            case 4:
+                System.out.println("División.");
+                break;
+            case 5:
+                System.out.println("Mayor.");
+                break;
+            case 6:
+                System.out.println("Menor.");
+                break;
+            case 7:
+                System.out.println("Potencia.");
+                break;
+            case 8:
+                System.out.println("Porcentaje.");
+                break;
+            case 9:
+                System.out.println("Raíz cuadrada.");
+                break;
+        }
     }
 
     private static void ecuacionCuadratica() {
